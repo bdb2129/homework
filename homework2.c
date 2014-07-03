483f7376d5b1f47b30d17a84f235e2cfe575d11a
bdb2129 <bdb2129@columbia.edu>
2014-07-03 16:14:27 -0400

    third homework

--- homework2.c
+++ homework2.c
@@ -4,15 +4,10 @@ int main()
 {
 	int x, y;
 
-	printf("Enter two integers that are divisible.\n");
+	printf("Enter two integers that are divisible. (X, X)\n");
 	scanf(" %d, %d", &x, &y);
 
-	if (x % y == 0) 
-	{
-		printf("These integers are divisible.\n");
-	}
-	else
-	{
-		printf("These integers are not divisible.\n");
-	}
+	x % y == 0 ? printf("Divisible.\n") : printf("Not divisible.\n"); 
+
+	return 0;
 }
