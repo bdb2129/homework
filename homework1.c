54c11366161f1e95201cfd92079d1296553eb487
bdb2129 <bdb2129@columbia.edu>
2014-07-03 16:13:50 -0400

    second homework

--- homework1.c
+++ homework1.c
@@ -3,23 +3,13 @@
 
 int main()
 {
-	int x, r[5];
-	int i = 0;
-	int z = r[4] + r[3] + r[2] + r[1] + r[0];
+	int x;
 
 	printf("Enter an integer. The system ");
 	printf("will find its octal equivalent.\n");
+	
 	scanf(" %d", &x);
+	printf("%o\n", x);
 
-	while (x > 8);
-	{
-		x = x/8;
-		r[i] = x % 8;
-		i++;
-	}
-	if (x <= 8)
-	{
-		printf("The octal equivalent is %d.\n", z);
-	}
 	return 0;
 }
