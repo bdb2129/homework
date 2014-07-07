8ce217fa89cc8aa1104dc5de80fa0223b1aa3ab9
bdb2129 <bdb2129@columbia.edu>
2014-07-07 15:57:00 -0400

    employee homework

--- homework3.c
+++ homework3.c
@@ -1,71 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "employee.h"
 
-typedef struct
+struct employee *create_employee()
 {
-	int x;
-	struct employee *next;
-	char name[100], phone[11], job[50], salary[7];
+	printf("Please input the name of the employee:\n");
+	scanf(" %s", employee.name);
 
-} employee;
+	printf("Please input the surname of the employee:\n");
+	scanf(" %s", employee.surname);
 
-void print_list(employee *n)
+	printf("Please input the job of the employee:\n");
+	scanf(" %s", employee.job);
+
+	printf("Please input the salary of the employee:\n");
+	scanf(" %d", employee.salary);
+};
+
+struct employee *print_list()
 {
-	while (n != NULL)
-	{
-		printf("%d\n", n->x);
-		n = n->next;
-	}
-}
-
-employee *create_employee(int cnt, employee *nxt)
-{
-	employee *new_employee;
-
-	new_employee = (employee *) malloc(168 * sizeof(employee));
-
-	new_employee->x = cnt;
-	new_employee->next = nxt;
-
-	return new_employee;
-
-}
-
-employee *add_to_front(employee *list, employee *new_employee)
-{
-	new_employee->next = list;
-
-	list = new_employee;
-
-	return list;
-
-}
+	printf(" %s", struct employee);
+};
 
 int main(int argc, const char *argv[])
-{
-	employee *list_head = NULL;
-	list_head = create_employee(4, NULL);
-	list_head = add_to_front(list_head, create_employee(6, NULL));
+{	
+	int answer;
 	
-	char a, b[100], name[100], phone[11], job[50], salary[7];
+	printf("Choose an option:\n");
+	printf("Type 0 to add a new employee.\n");
+	printf("Type 1 to print the list of employees.\n");
+	scanf(" %d", answer);
 
-	printf("Do you want to add a new employee ");
-	printf("or print the current employees?\n");
-	printf("Enter \"A\" or \"P\".\n");
-	scanf(" %c", &a);
-
-	if (strcmp(a, "A") == 0)
+	switch(answer)
 	{
-		printf("Enter the name, phone, job, and salary ");
-		printf("of the employee (X, X, X, X).\n");
-		scanf(" %s, %s, %s, %s", name, phone, job, salary);
+	case 0:
+	
+		printf(" %p", struct emoloyee *create_employee());		
+		break;
 
-		create_employee;	
-	}
-
-	if (strcmp(b, "P") == 0)
-	{
-		print_list(list_head);
+	case 1:
+	
+		printf(" %p", struct employee *print_list());
+		
+		break;
 	}
 	
 	return 0;
